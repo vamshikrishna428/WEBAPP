f7aae886e8d554a66b141f594ec914fbbfc7b231
vamshikrishna428 <vamshikrishnakr@gmail.com>
2023-03-26 21:07:37 +0530

    second commit

--- src/test/java/IccScenarios/Scenario1.java
+++ src/test/java/IccScenarios/Scenario1.java
@@ -4,20 +4,33 @@ import java.time.Duration;
 import java.util.Set;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Point;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.Test;
-
-import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Scenario1 {
 	@Test
 	public void scenario1() {
-		WebDriverManager.chromedriver().setup();
-		WebDriver driver=new ChromeDriver();
+/*		WebDriverManager.chromedriver().setup();
+		ChromeOptions options=new ChromeOptions();
+		options.addArguments("--remote-allow-origins=*");
+		WebDriver driver=new ChromeDriver(options);
+		driver.get("https://www.flipkart.com/");
+
+*/		
+		
+//		WebDriverManager.chromedriver().setup();
+		ChromeOptions options=new ChromeOptions();
+		options.addArguments("--remote-allow-origins=*");
+		WebDriver driver=new ChromeDriver(options);
 		driver.manage().window().maximize();
+//		Point p=new Point(500, 500); 
+//		driver.manage().window().setPosition(p); 
+
 		driver.get("https://www.t20worldcup.com/");
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		driver.findElement(By.xpath("//child::li[@class='site-tabs__item theme theme-icc']/child::a")).click();
@@ -34,6 +47,7 @@ public class Scenario1 {
 		driver.findElement(By.xpath("//ancestor::nav[@class='main-navigation__desktop-list js-desktop-nav']/child::ul[@class='linked-list js-dynamic-list']/child::li[@class='linked-list__item  has-children ']/descendant::li[@class='linked-list__item   'and contains(.,'Player Rankings')]")).click();
 		
 		driver.quit();
+		
 	}
 	
 }
--- src/test/java/IccScenarios/Scenario2.java
+++ src/test/java/IccScenarios/Scenario2.java
@@ -5,13 +5,11 @@ import java.time.Duration;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Scenario2 {		// YOUTUBE SONG 
-	@Test
-	public void scenario2() {
+	public static void main(String[] args) {
 		WebDriverManager.chromedriver().setup();
 		WebDriver driver=new ChromeDriver();
 		driver.manage().window().maximize();
